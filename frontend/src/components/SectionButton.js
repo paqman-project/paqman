10a8c190c04af92adac63327f85cbcc1113f2ebb
nweber <weber.nadine09@gmail.com>
2021-03-19 16:06:46 +0100

    SectionButton hover added & name changed

--- frontend/src/components/SectionButton.js
+++ frontend/src/components/SectionButton.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function SectionButton(){
     return(
         <>
-            <div className="bg-white border-2 rounded-lg p-2 mx-2 my-2 shadow-md">
-                <h1 className="text-center ">Klick mich!</h1>
+            <div className="bg-white border-2 rounded-lg p-2 mx-2 my-2 shadow-md hover:border-gray-500">
+                <h1 className="text-center">Click me!</h1>
             </div>
         </>
     )
