6082ea7d771cd5a2c4392f29ba38f7a59a5b344a
nweber <weber.nadine09@gmail.com>
2021-03-19 16:34:40 +0100

    SectionButton cursor changed to pointer

--- frontend/src/components/SectionButton.js
+++ frontend/src/components/SectionButton.js
@@ -3,7 +3,8 @@ import React from "react"
 export default function SectionButton(){
     return(
         <>
-            <div className="bg-white border-2 rounded-lg p-2 mx-2 my-2 shadow-md hover:border-gray-500">
+            <div className="bg-white border-2 rounded-lg p-2 mx-2 my-2 
+                            shadow-md hover:border-gray-500 cursor-pointer">
                 <h1 className="text-center">Click me!</h1>
             </div>
         </>
