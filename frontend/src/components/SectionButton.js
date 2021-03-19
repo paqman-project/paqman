5588774f888b3f45b8a0459b8ee9b1b846851281
nweber <weber.nadine09@gmail.com>
2021-03-19 15:46:22 +0100

    Section Buttons centered and margin changed

--- frontend/src/components/SectionButton.js
+++ frontend/src/components/SectionButton.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function SectionButton(){
     return(
         <>
-            <div className="bg-white border rounded-lg p-2 m-2 w-2/3 ml-4">
+            <div className="bg-white border-2 rounded-lg p-2 mx-2 my-2 shadow-md">
                 <h1 className="text-center ">Klick mich!</h1>
             </div>
         </>
