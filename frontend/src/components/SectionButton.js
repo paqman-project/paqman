12762641618be5743069c6791b2d52fb645b213f
nweber <weber.nadine09@gmail.com>
2021-03-19 19:03:25 +0100

    Button changed into h1 (no frame border)

--- frontend/src/components/SectionButton.js
+++ frontend/src/components/SectionButton.js
@@ -6,7 +6,7 @@ export default function SubSectionButton({setView, title}){
             <div className="bg-white border-2 rounded-lg p-2 mx-2 my-2 
                             shadow-md hover:border-gray-500 cursor-pointer content-center"
                 onClick={() => setView(title)}>
-                <button className="text-center">{title}</button>
+                <h1 className="text-center ">{title}</h1>
             </div>
         </>
     )
