2d3f8ad99c31b015c19e512cb317cd9e068b50a6
Leon Schmidt <mail@leon.wtf>
2021-04-02 13:19:39 +0200

    Added React Router and file watcher excludes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from "react"
 import Header from "./container/Header"
 import Body from "./container/Body"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import AboutView from "./views/AboutView"
 
 export default function App() {
     return (
         <div className="h-full w-full absolute top-0">
             <Header />
-            <Body />
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Body} />
+                    <Route path="/test" component={AboutView} />
+                </Switch>
+            </Router>
         </div>
     )
 }
