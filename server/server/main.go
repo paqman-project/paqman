b1f7c370b06d6d3064aabd5d73eb5ac4b4d9abdd
Leon Schmidt <mail@leon.wtf>
2021-04-03 16:20:13 +0200

    Added static frontend routes

--- server/server/main.go
+++ server/server/main.go
@@ -9,20 +9,39 @@ import (
 	"paqman-backend/config"
 )
 
+var frontendRoutes = []string{
+	"/static",
+	"/manifest.json",
+	"/asset-manifest.json",
+	"/favicon.ico",
+	"/robots.txt",
+}
+
 // Start starts the API and frontend server.
 // This function is blocking.
 func Start() error {
+
 	// router
 	router := mux.NewRouter()
+
 	// API routes
 	apiRouter := router.PathPrefix("/api").Subrouter()
 	apiRouter.HandleFunc("/ping", pingHandler).Methods("GET")
-
-	// Get test value
+	// TEMP get test value
 	apiRouter.HandleFunc("/show", showHandler).Methods("GET")
+	apiRouter.PathPrefix("/").HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		respondString(&rw, "Endpoint not found", 404)
+	})
 
-	// Frontend route
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("../frontend/build")))
+	// frontend routes
+	// add static routes from frontendRoutes to be redirected to the frontend
+	for _, route := range frontendRoutes {
+		router.PathPrefix(route).Handler(http.FileServer(http.Dir("../frontend/build")))
+	}
+	// catch-all router that always serves the build directory to make the react browser router work
+	router.PathPrefix("/").HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		http.ServeFile(rw, r, "../frontend/build")
+	})
 
 	// start server
 	server := &http.Server{
@@ -31,4 +50,5 @@ func Start() error {
 	}
 	log.Println("Starting server...")
 	return server.ListenAndServe() // ListenAndServer never returns a non-nil error
+
 }
