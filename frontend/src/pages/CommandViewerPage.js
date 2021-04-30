d1ab1057561ddf248fc3d8e8a4330ce31b163d87
Leon Schmidt <mail@leon.wtf>
2021-04-30 19:39:10 +0200

    Instructions and CommandProperties now scale properly

--- frontend/src/pages/CommandViewerPage.js
+++ frontend/src/pages/CommandViewerPage.js
@@ -11,6 +11,7 @@ import Instructions from "../components/Instructions"
  */
 export default function CommandViewerPage({ match }) {
     
+    // get the commandID from route URL
     const { params: { commandID } } = match
 
     const [ data, setData ] = useState()
@@ -20,42 +21,35 @@ export default function CommandViewerPage({ match }) {
             .then(r => r.json())
             .then(r => setData(r))
             .catch(e => console.log(e))
-    }, [ commandID ]) // TODO don't know if this is correct
+    }, [ commandID ])
 
-    const inner = () => {
-        if (!data) return <Loading />
-        if (data.error) return <p>{data.error}</p>
-        return (
-            <div>
-                <ViewHeading 
-                    title={data.name[0].toUpperCase() + data.name.substring(1)}
-                    subtitle={data.description}
-                />
-                <div className="max-w-min"> {/* Do not allow the instruction component to grow endlessly */}
-                    <div className="w-5/6 mx-auto flex space-x-20 mb-10">
-                        <div className="flex-1"> {/* .flex-1 allows the div to grow and shrink to "just fit". As this is a flexbox, Instructions and CommandProperties will have the same size */}
-                            <Instructions
-                                text={"Unused, until #71 is resolved"}
-                            />
-                        </div>
-                        <div className="flex-1">
-                            <CommandProperties 
-                                properties={data}
-                            />
-                        </div>
-                    </div>
-                    <CommandTemplateForm
-                        template={data.template}
-                        templateValues={data.template_values}
-                    />
-                </div>
-            </div>
-        )
-    }
+    if (!data) return <Loading />
+    if (data.error) return <p>{data.error}</p>
 
     return (
-        <div className="h-full w-full flex justify-center p-4">
-            { inner() }
+        <div className="h-full w-full p-4">
+            <ViewHeading 
+                title={data.name[0].toUpperCase() + data.name.substring(1)}
+                subtitle={data.description}
+            />
+            <div>
+                <div className="w-5/6 max-w-5xl mx-auto flex space-x-20 mb-10">
+                    <div className="flex-1"> {/* .flex-1 allows the div to grow and shrink to "just fit". As this is a flexbox, Instructions and CommandProperties will have the same size */}
+                        <Instructions
+                            text={"Unused, until #71 is resolved. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam"} // TODO
+                        />
+                    </div>
+                    <div className="flex-1">
+                        <CommandProperties 
+                            properties={data}
+                        />
+                    </div>
+                </div>
+                <CommandTemplateForm
+                    template={data.template}
+                    templateValues={data.template_values}
+                />
+            </div>
         </div>
     )
 }
