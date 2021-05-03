68fc080f245073ecf30900cbd1f35969ad3e6306
nweber <weber.nadine09@gmail.com>
2021-05-03 20:00:54 +0200

    Added Padding to Logo

--- frontend/src/container/Header.js
+++ frontend/src/container/Header.js
@@ -7,7 +7,7 @@ import "../logo_v2.svg";
 export default function Header() {
     return (
         <div className="h-16 flex justify-between items-center shadow-lg">
-            <img src="/logo.svg" width="350px" alt="PAQMAN Logo" className="mx-4"/>
+            <img src="/logo.svg" width="350px" alt="PAQMAN Logo" className="mx-2 p-2"/>
         </div>
     )
 }
