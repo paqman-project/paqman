cb6172ac7b2d3863f172c65febc785e8813cd69c
nweber <weber.nadine09@gmail.com>
2021-05-03 23:13:11 +0200

    About Page with different Font Styles

--- frontend/src/views/preparation/AboutView.js
+++ frontend/src/views/preparation/AboutView.js
@@ -6,7 +6,16 @@ export default function AboutView() {
     return (
         <div>
             <ViewHeading title="About PAQMAN" />
-            <NotImplemented />
+            <p className="mx-40 p-4 border-paqgreen-200 shadow-lg shadow-gray">
+                <p className="">Default: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br/></p>
+                <p className="p-2"></p>
+                <p className="font-sans">Sans: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br/></p>
+                    <p className="p-2"></p>
+                <p className="font-serif">Serif: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br/></p>
+                    <p className="p-2"></p>
+                <p className="font-mono">Mono: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br/></p>
+            </p>
+            
         </div>
     )
 }
