051653dc1f68264878174cd7f4a98c6c5b9c1e93
Leon Schmidt <mail@leon.wtf>
2021-05-07 22:35:55 +0200

    Added APISearchbar, adjusted Card component

---
+++ frontend/src/components/APISearchbar.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react"
+
+/**
+ * Uses the /search enpoint to search for commands, parameters or attacks.
+ * THIS IS CURRENTLY UNUSED ANY MAY NOT WORK
+ * @param props
+ * @param props.type Can be one of ("command", "parameter", "attack"). If not set, a global search is performed
+ */
+export default function APISearchbar({ type }) {
+
+    const [ term, setTerm ] = useState()
+
+    const handleChange = (event) => {
+        console.log(event.target.value)
+    }
+
+    useEffect(() => {
+        console.log("used effect")
+    }, [term])
+
+    return (
+        <div>
+            <input 
+                type="text"
+                value={term}
+                onChange={handleChange}
+                placeholder={`Search for ${type}s`}
+            />
+        </div>
+    )
+}
--- frontend/src/pages/CommandViewerPage.js
+++ frontend/src/pages/CommandViewerPage.js
@@ -35,12 +35,12 @@ export default function CommandViewerPage({ match }) {
             <div>
                 <div className="w-5/6 max-w-5xl mx-auto flex space-x-20 mb-10">
                     <div className="flex-1">
-                        <Card title="Instructions">
+                        <Card title="Instructions" fullHight>
                             <p>{data.instructions}</p>
                         </Card>
                     </div>
                     <div className="flex-1">
-                        <Card title="Command properties">
+                        <Card title="Command properties" fullHight>
                             <p>
                                 Requires root?<span> </span>
                                 {data.requires_root ? (
