45db4f86c1d0811cf470653e430ff0829fa8840c
Leon Schmidt <mail@leon.wtf>
2021-05-15 23:25:23 +0200

    Test performance adjustments

--- server/db/main.go
+++ server/db/main.go
@@ -26,6 +26,8 @@ var Client *Mongo
 // No-SQL approach
 type MongoCRUD interface {
 	CreateOne(string, interface{}) (primitive.ObjectID, error)
+	// CreateMany is not defined as it is simpler to handle
+	// ObjectIDs when calling CreateOne multiple times
 	ReadOne(string, bson.M, interface{}) error
 	ReadMany(string, bson.M) (*mongo.Cursor, error)
 	// Update(string, bson.M, ...) // TODO
@@ -53,17 +55,25 @@ var _ MongoCRUD = (*Mongo)(nil)
 // is called before Connect().
 func Connect(mocked bool) error {
 
-	connectString := fmt.Sprintf("mongodb://%s:%s@%s/?authSource=admin", config.Current.MongoDBUser, config.Current.MongoDBPass, config.Current.MongoDBAddress)
-	connectStringNoPass := fmt.Sprintf("mongodb://%s:%s@%s/?authSource=admin", config.Current.MongoDBUser, "***", config.Current.MongoDBAddress)
-
-	log.Printf("Connecting to MongoDB at %s\n", connectStringNoPass)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
 	var client *mongo.Client
 
 	if !mocked { // only establish a real connection, if the database should not be mocked
+
+		connectString := fmt.Sprintf("mongodb://%s:%s@%s/?authSource=admin",
+			config.Current.MongoDBUser,
+			config.Current.MongoDBPass,
+			config.Current.MongoDBAddress,
+		)
+		connectStringNoPass := fmt.Sprintf("mongodb://%s:***@%s/?authSource=admin",
+			config.Current.MongoDBUser,
+			config.Current.MongoDBAddress,
+		)
+
+		log.Printf("Connecting to MongoDB at %s\n", connectStringNoPass)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
 		var err error
 		client, err = mongo.Connect(ctx, options.Client().ApplyURI(connectString))
 		if err != nil {
@@ -72,7 +82,9 @@ func Connect(mocked bool) error {
 		if err := client.Ping(ctx, nil); err != nil {
 			return err
 		}
+
 		log.Println("Connected to MongoDB!")
+
 	}
 
 	Client = &Mongo{
@@ -86,9 +98,14 @@ func Connect(mocked bool) error {
 // if it is still reachable. Returns nil, if it is
 // reachable or returns the error otherwise
 func (m *Mongo) CheckConnection() error {
-	if err := m.connection.Ping(context.TODO(), nil); err != nil {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second) // TODO maybe this is not sufficient
+	defer cancel()
+
+	if err := m.connection.Ping(ctx, nil); err != nil {
 		return err
 	}
+
 	return nil
 }
 
