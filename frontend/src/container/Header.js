ed1c92ccd488284a3663c0d17d9bcd0d89f46d86
nweber <weber.nadine09@gmail.com>
2021-06-05 11:43:17 +0200

    Logo is changing on hover

--- frontend/src/container/Header.js
+++ frontend/src/container/Header.js
@@ -1,13 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 import logo from "../img/logo.svg"
+import logo2 from "../img/logo_v2.svg"
 
 /**
  * This is PAQMANs header for all pages
  */
 export default function Header() {
+    const [hovering, setHovering] = useState(false)
+
     return (
         <div className="h-16 flex justify-between items-center shadow-lg">
-            <img src={logo} width="300px" alt="PAQMAN Logo" className="p-6" />
+            <Link to="/">
+                <div
+                    onMouseOver={() => setHovering(true)}
+                    onMouseLeave={() => setHovering(false)}
+                >
+                    {hovering ? (
+                        <img
+                            src={logo2}
+                            width="300px"
+                            alt="PAQMAN Logo"
+                            className="p-6"
+                        />
+                    ) : (
+                        <img
+                            src={logo}
+                            width="300px"
+                            alt="PAQMAN Logo with Slogan"
+                            className="p-6"
+                        />
+                    )}
+                </div>
+            </Link>
         </div>
     )
 }
