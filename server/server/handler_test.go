4555fe076c9d3e761edc3222c6d2e92055c26233
Leon Schmidt <mail@leon.wtf>
2021-06-09 20:39:34 +0200

    Added parameter example and unit tests

--- server/server/handler_test.go
+++ server/server/handler_test.go
@@ -77,3 +77,43 @@ func TestNewCommandHandler(t *testing.T) {
 	}
 
 }
+
+func TestNewParameterHandler(t *testing.T) {
+
+	if err := db.Connect(true); err != nil {
+		t.Fatal("Could not create the mocked database")
+	}
+
+	// this is intended to always work!
+	j, _ := json.Marshal(structs.ExampleParameterFVEK)
+
+	// request that will be tested
+	req, err := http.NewRequest("POST", "/api/parameter", bytes.NewReader(j))
+	if err != nil {
+		t.Fatalf("Error creating the request: %s", err.Error())
+	}
+
+	// test server that uses the handler that will be tested
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(newParameterHandler)
+	handler.ServeHTTP(rec, req)
+
+	// check status code
+	expectedSC := 201
+	if sc := rec.Code; sc != expectedSC {
+		t.Errorf("Handler returned wrong status code: got %v want %v", sc, expectedSC)
+	}
+
+	// check if body contains a new ObjectID
+	var body struct {
+		ID string `json:"_id"`
+	}
+	err = json.NewDecoder(rec.Body).Decode(&body)
+	if err != nil {
+		t.Errorf("Error while decoding the body: %s", err.Error())
+	}
+	if body.ID == "" {
+		t.Error("Couldn't get new Object ID")
+	}
+
+}
