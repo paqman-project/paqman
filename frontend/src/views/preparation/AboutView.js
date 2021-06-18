ed2cc94f30f96d0608e056764b9c8b740587e0e5
Leon Schmidt <mail@leon.wtf>
2021-06-18 21:35:06 +0200

    Initial about page contents

--- frontend/src/views/preparation/AboutView.js
+++ frontend/src/views/preparation/AboutView.js
@@ -1,46 +1,79 @@
 import React from "react"
-import NotImplemented from "../../components/NotImplemented"
 import ViewHeading from "../../components/ViewHeading"
+import Card from "../../components/Card"
+import logo from "../../img/logo_v2.svg"
 
 export default function AboutView() {
     return (
-        <div>
-            <ViewHeading title="Font Styles" />
-            <NotImplemented plannedFor="v0.2.1" />
-            <p className="mx-20 p-4 border-paqgreen-200 shadow-lg shadow-gray">
-                <p className="p-2">
-                    font-sans: Lorem ipsum dolor sit amet, consetetur sadipscing
-                    elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-                    dolore magna aliquyam erat, sed diam voluptua. At vero eos
-                    et accusam et justo duo dolores et ea rebum. Stet clita kasd
-                    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-                    amet.
-                </p>
-                <p className="font-serif p-2">
-                    font-serif: Lorem ipsum dolor sit amet, consetetur
-                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                    labore et dolore magna aliquyam erat, sed diam voluptua. At
-                    vero eos et accusam et justo duo dolores et ea rebum. Stet
-                    clita kasd gubergren, no sea takimata sanctus est Lorem
-                    ipsum dolor sit amet.
-                </p>
-                <p className="font-mono p-2">
-                    font-mono: Lorem ipsum dolor sit amet, consetetur sadipscing
-                    elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-                    dolore magna aliquyam erat, sed diam voluptua. At vero eos
-                    et accusam et justo duo dolores et ea rebum. Stet clita kasd
-                    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-                    amet.
-                </p>
-                <p className="font-raleway p-2">
-                    font-raleway: Lorem ipsum dolor sit amet, consetetur
-                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                    labore et dolore magna aliquyam erat, sed diam voluptua. At
-                    vero eos et accusam et justo duo dolores et ea rebum. Stet
-                    clita kasd gubergren, no sea takimata sanctus est Lorem
-                    ipsum dolor sit amet.
-                </p>
-            </p>
+        <div className="font-raleway">
+            {/*
+                <ViewHeading title="About PAQMAN" />
+            */}
+
+            {/* Logo */}
+            <div className="w-2/3 mx-auto max-w-md mt-20">
+                <img src={logo} alt="PAQMAN logo full" />
+            </div>
+
+            {/* Important infos (subtitle) */}
+            <div className="p-2 m-4 text-center text-lg">
+                <p>Version v0.1.0</p>
+            </div>
+
+            {/* Cards with information */}
+            <div className="w-5/6 mx-auto mt-12">
+                <div className="space-y-10">
+                        <Card title="What is PAQMAN?">
+                            <div className="text-center">
+                                Mehr Senf
+                            </div>
+                        </Card>
+                        <Card title="Terminology">
+                            <table className="text-left">
+                                <tr>
+                                    <td className="font-bold">Command: </td>
+                                    <td className="p-2 pl-4">
+                                        Well, just a command. This can be a binary, shell or any 
+                                        other executable with flags, values, etc.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="font-bold">Attack: </td>
+                                    <td className="p-2 pl-4">
+                                        An Attack bundles Commands statically to form a predefined 
+                                        sequence of actions. The Commands are preconfigured and
+                                        ready to be executed.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="font-bold">Parameter: </td>
+                                    <td className="p-2 pl-4">
+                                        This is PAQMANs centerpiece. Parameters define abstract
+                                        subgoals while executing a chain of Commands (e.g. an Attack).
+                                        They provide context to usually stateless Commands.
+                                    </td>
+                                </tr>
+                            </table>
+                        </Card>
+                        <Card title="How do you use it?">
+                            <div>
+                                <p>Senf</p>
+                            </div>
+                        </Card>
+                        <Card title="Who made it?">
+                            <div className="text-center">
+                                <p>
+                                    PAQMAN was developed as a project work by
+                                    Nadine Weber, Nicola Jäger and Leon Schmidt
+                                    as part of a project work at the University
+                                    of Applied Siences Offenburg.
+                                </p>
+                            </div>
+                        </Card>
+                </div>
+
+            </div>
+
         </div>
     )
 }
