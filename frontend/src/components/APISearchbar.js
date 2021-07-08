e26a0011e6f0c34d3c0ae1d526d666bdd2cf04e2
Leon Schmidt <mail@leon.wtf>
2021-07-08 22:15:49 +0200

    Added comments, began with first overlay implementation

--- frontend/src/components/APISearchbar.js
+++ frontend/src/components/APISearchbar.js
@@ -1,43 +1,48 @@
-import React, { useEffect, useState } from "react"
-import NotImplemented from "./NotImplemented"
+import React, { useState } from "react"
 
 /**
  * Uses the /search enpoint to search for commands, parameters or attacks.
  * THIS IS CURRENTLY UNUSED AND DOES NOT WORK
  * @param props
  * @param props.searchFor Can be one of ("command", "parameter", "attack"). If not set, a global search is performed
- * @param props.overlay A function with signature `function (results): React.Component`. It receives the results function
- * from the backend
+ * @param props.overlay A function with signature `function (results): React.Component`. It receives the results
+ * from the database and is used to provide customized layout of the result representation 
  */
 export default function APISearchbar({ searchFor, overlay }) {
     // search term that the user searches for
     const [term, setTerm] = useState("")
     // timeout that triggers the fetch /api/search call, when the user stops typing 
     const [searchTimer, setSearchTimer] = useState(null)
-    // seach results
-    const [results, setResults] = useState()
+    // seach results (default null, because should be nullable)
+    const [results, setResults] = useState(null)
 
     // TODO check searchFor?
 
     const handleChange = event => {
 
+        let value = event.target.value
+
         clearTimeout(searchTimer)
         setSearchTimer(null) // this may cause errors!
         
-        setTerm(event.target.value)
+        setTerm(value)
         
-        let t = setTimeout(() => {
-            if (term === "") return
+        let timeout = setTimeout(() => {
+            // hide results panel, if search term is empty
+            if (value === "") {
+                setResults(null)
+                return
+            }
             // compose query string
             let p = new URLSearchParams()
-            p.set("term", term)
+            p.set("term", value)
             if (searchFor) {
                 p.set("searchFor", searchFor)
             }
             // request and set results
             ////// TEMP START //////
             console.log(`Fetching /api/search?${p.toString()}`)
-            setResults(JSON.stringify({
+            setResults({
                 "commands": [
                     {
                         "_id": "1337",
@@ -58,33 +63,46 @@ export default function APISearchbar({ searchFor, overlay }) {
                     }
                 ],
                 "attacks": null
-            }))
+            })
             ////// TEMP END //////
             /*fetch(`/api/search?${p.toString()}`)
                 .then((r) => r.json())
                 .then((r) => setResults(r))
                 .catch((e) => console.log(e))*/
-        }, 1000)
-        setSearchTimer(t)
+        }, 500)
+        setSearchTimer(timeout)
 
     }
 
+    const doOverlay = () => {
+        try {
+            return overlay(results)
+        } catch (e) {
+            console.error("Error while executing the \"overlay\" function: ", e)
+            return <p className="text-red-500 text-center">ERROR</p>
+        }
+    }
+
     return (
-        <div>
+        <div className="relative w-full">
             <input
                 type="text"
                 value={term}
                 onChange={handleChange}
-                placeholder={`Search for ${searchFor}`}
-                className="px-4 py-2 border rounded-lg shadow-lg"
+                placeholder={`Search for ${searchFor || "everything"}`}
+                className="relative w-full px-4 py-2 border rounded-lg shadow-lg z-20"
             />
             {results && (
-                <div className="absolute top-0 left-0 right-0 bg-red-500 rounded">
-                    {
-                        overlay
-                            ? overlay(results)
-                            : <p>No overlay function</p>
-                    }
+                <div className="absolute w-full z-10">
+                    <div className="-mt-4 mx-4 bg-white shadow-md border rounded-b-lg">
+                        <div className="mt-4 p-4">
+                            {
+                                overlay
+                                    ? doOverlay()
+                                    : <p>No overlay function</p>
+                            }
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
--- frontend/src/views/preparation/CommandByParameterView.js
+++ frontend/src/views/preparation/CommandByParameterView.js
@@ -1,6 +1,7 @@
 import React from "react"
-import NotImplemented from "../../components/NotImplemented"
+import APISearchbar from "../../components/APISearchbar"
 import ViewHeading from "../../components/ViewHeading"
+import Button from "../../components/Button"
 
 /**
  * This view is used to search commands by parameters
@@ -9,7 +10,71 @@ export default function CommandByParameterView() {
     return (
         <div>
             <ViewHeading title="Search commands by parameter" />
-            <NotImplemented plannedFor="v0.2.1" />
+            <div>
+                <APISearchbar 
+                    searchFor="commands" 
+                    overlay={(results) => (
+                        <div>
+                            {results.commands && (
+                                <>
+                                    <div>
+                                        <h1 className="font-bold">Commands</h1>
+                                        {results.commands?.map(c => 
+                                            <div className="border m-2 p-2 rounded-md">
+                                                <div className="flex flex-row justify-between items-center">
+                                                    <div>
+                                                        <p>
+                                                            <span className="text-lg font-mono bg-gray-100 rounded px-2 py-1">
+                                                                {c.name}
+                                                            </span> {c.description}
+                                                        </p>
+                                                    </div>
+                                                    <div className="flex flex-row space-x-5">
+                                                        <div>
+                                                            <Button title="Add to entrypoints" />
+                                                        </div>
+                                                        <div>
+                                                            <Button title="Set target" />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                </>
+                            )}
+                            {results.attacks && (
+                                <>
+                                    <hr className="mt-4 mb-2" />
+                                    <div>
+                                        <h1 className="font-bold">Attacks</h1>
+                                        {results.attacks.map(c => (
+                                            <div className="border m-2 p-2">
+                                                <h2>{c.name}</h2>
+                                                <p>{c.description}</p>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </>
+                            )}
+                            {results.parameters && (
+                                <>
+                                    <hr className="mt-4 mb-2" />
+                                    <div>
+                                        <h1 className="font-bold">Parameters</h1>
+                                            {results.parameters?.map(c => (
+                                                <div className="border m-2 p-2">
+                                                    <h2>{c.name}</h2>
+                                                    <p>{c.description}</p>
+                                                </div>
+                                            ))}
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    )}
+                />
+            </div>
         </div>
     )
 }
